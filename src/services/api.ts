5c1904171d792f11b4e298a4cd68886dd66d95c0
Kirionenko <misha.kirionenko03@gmail.com>
2023-10-27 00:33:43 +0500

    Истина где-то на сервере

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,9 +3,8 @@ import axios, { AxiosInstance } from 'axios';
 const BACKEND_URL = 'https://13.design.pages.academy/wtw';
 const REQUEST_TIMEOUT = 5000;
 
-export const createAPI = (): AxiosInstance => {
-  return axios.create({
+export const createAPI = (): AxiosInstance =>
+  axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
-};
