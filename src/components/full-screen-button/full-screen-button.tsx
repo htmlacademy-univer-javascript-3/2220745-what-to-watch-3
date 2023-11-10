f39caad87652f85fb74b754f6d7b88e76ca135eb
Kirionenko <misha.kirionenko03@gmail.com>
2023-11-11 02:44:38 +0500

    8.13. Оптимизируй это (часть 2)

---
+++ src/components/full-screen-button/full-screen-button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type FullScreenProps = {
+  onClick: () => void;
+};
+
+function FullScreen({ onClick }: FullScreenProps) {
+  return (
+    <button type="button" className="player__full-screen" onClick={onClick}>
+      <svg viewBox="0 0 27 27" width="27" height="27">
+        <use xlinkHref="#full-screen"></use>
+      </svg>
+      <span>Full screen</span>
+    </button>
+  );
+}
+
+export default React.memo(FullScreen);
